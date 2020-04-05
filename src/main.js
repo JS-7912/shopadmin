e05c4133d507d348c32ff14fe973c8a4960b1b7d
JS_12 <1136230783@qq.com>
2020-04-05 18:20:52 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// 忽略：项目提示
+Vue.config.productionTip = false
+
+// 配置ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+// 配置全局默认样式&图标（@代表src目录
+import '@/assets/css/reset.scss'
+import '@/assets/font/iconfont.css'
+
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app')
